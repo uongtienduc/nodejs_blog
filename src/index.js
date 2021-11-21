9f9dc94e48a58d47be9cd63f02a63d144aea8732
uongtienduc <traanfhwng@gmail.com>
2021-11-21 19:26:27 +0700

    3

--- src/index.js
+++ src/index.js
@@ -2,29 +2,39 @@ const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
 var handlebars = require('express-handlebars');
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+
 
 //Template engine
 var hbs = handlebars.create({
   extname: 'hbs'
 });
 
+app.use(express.static('./src/resources/public'));
+
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set("views", path.join(__dirname, 'resources/views'));
 
+
+
 //HTTP logger
-app.use(morgan('combined'))
+//app.use(morgan('combined'))
 
 app.get('/', (req, res) => {
     res.render('home');
-})
+});
 
 app.get('/news', (req, res) => {
   res.render('news');
-})
+});
+
+app.get('/search', (req, res) => {
+  console.log(req.query);
+  res.render('search');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
